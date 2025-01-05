19f79d107d8dd7fee487d7a40947619ada56aef5
iyadelwy <iyadelwy@gmail.com>
2025-01-05 20:00:25 +0100

    add movies table sql

---
+++ portfolio/db-init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Movies(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    year TEXT,
+    rated TEXT,
+    released TEXT,
+    runtime TEXT,
+    genre TEXT,
+    director TEXT,
+    writer TEXT,
+    actors TEXT,
+    plot TEXT,
+    language TEXT
+)
