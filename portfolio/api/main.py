c6665dd1d9f44d9e9b7678bf85ad7c354cb55453
iyadelwy <iyadelwy@gmail.com>
2025-01-09 14:29:14 +0100

    add initial logging

--- portfolio/api/main.py
+++ portfolio/api/main.py
@@ -1,17 +1,29 @@
 import requests
 from cachetools import TTLCache
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from requests.auth import HTTPBasicAuth
 from dotenv import dotenv_values
+from loki_logger_handler.loki_logger_handler import LokiLoggerHandler
+import logging
+import os
+import time
 
 config = dotenv_values(".env")
 
 
+logger = logging.getLogger("web-api-logger")
+logger.setLevel(logging.DEBUG)
+custom_logging_handler = LokiLoggerHandler(
+        url="http://loki:3100/loki/api/v1/push",
+            labels={"application": "portfolio", "component": "web-api"}
+)
+logger.addHandler(custom_logging_handler)
+
+
 class CommandBody(BaseModel):
     command: str
-
 
 class Movie(BaseModel):
     title: str
@@ -20,7 +32,30 @@ class Movie(BaseModel):
 app = FastAPI()
 cache = TTLCache(maxsize=15, ttl=180)
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    client_ip = request.client.host
+    method = request.method
+    path = request.url.path
+    query_params = dict(request.query_params)
+    user_agent = request.headers.get("user-agent", "unknown")
+    try:
+        body = await request.body()
+        body = body.decode("utf-8") if body else "empty"
+    except Exception:
+        body = body.decode("utf-8") if body else "empty"
+        
 
+    response = await call_next(request) 
+
+    response_time = time.time() - start_time    
+    status_code = response.status_code
+
+    log_string = f"[{timestamp}] | Method: {method} | Path: {path} | Query Params: {query_params} | Body: {body} | Client IP: {client_ip} | User-Agent: {user_agent} | Status Code: {status_code} | Response Time: {response_time:.2f}s"
+    logging.info(log_string)
+    
+    return response
+    
 @app.post("/cmd")
 def cmd(command_body: CommandBody):
     res = requests.post(
